ea6e4a96088819f3336c55de79c72a1f42cb0adb
Nikica <markovskinikica@gmail.com>
2020-10-22 23:31:53 +0200

    create mySql database

---
+++ solution/backend/sql.sql
@@ -0,0 +1,21 @@
+-- CREATE TABLE IF NOT EXISTS User (
+--   user_id INT AUTO_INCREMENT PRIMARY KEY ,
+-- 	username varchar(255) not null,
+-- 	password varchar(255) not null
+--  );
+ 
+-- CREATE TABLE IF NOT EXISTS Category (
+--   category_id INT AUTO_INCREMENT PRIMARY KEY ,
+-- 	name varchar(55) not null
+-- );
+ 
+--  CREATE TABLE IF NOT EXISTS Post (
+--   post_id INT AUTO_INCREMENT PRIMARY KEY ,
+--     title varchar(255) not null,
+--     content varchar(255) not null,
+--    user_id int not null,
+--    category_id int not null ,
+--    foreign key  (user_id) references User(user_id) on delete cascade,
+--    foreign key  (category_id) references Category(category_id) on delete cascade
+--  );
+ 
